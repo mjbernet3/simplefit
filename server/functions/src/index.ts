b55ecf88839a90a855f7d79382b53c0723505b33
Matthew Bernet <mattbernetGT@gmail.com>
2020-06-13 22:52:20 -0400

    Configure admin sdk, add func to remove profile on account deletion

--- server/functions/src/index.ts
+++ server/functions/src/index.ts
@@ -1,8 +1,21 @@
 import * as functions from 'firebase-functions';
+import admin = require('firebase-admin');
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Only initialize here
+admin.initializeApp({
+    credential: admin.credential.applicationDefault(),
+    databaseURL: 'https://simplefit-2020.firebaseio.com'
+  });
+
+const db = admin.firestore();
+
+// Functions reference
+// https://firebase.google.com/docs/functions/typescript
+
+export const deleteUserProfile = functions.auth.user().onDelete(async (user) => {
+    try {
+        await db.collection('users').doc(user.uid).delete();
+    } catch (error) {
+        console.log(error);
+    }
+});
